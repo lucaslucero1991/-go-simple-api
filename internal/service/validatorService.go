2349377937fdbf7a29ea6a08342358b745786266
llucero <lucaslucero1991@gmail.com>
2024-05-14 07:53:15 -0300

    Add validations service test

--- internal/service/validatorService.go
+++ internal/service/validatorService.go
@@ -5,6 +5,13 @@ import (
 	"v0/internal/delivery/http/request"
 )
 
+const (
+	ErrMissingNameField    = "missing name field in job request"
+	ErrMissingCountryField = "missing country field in job request"
+	ErrMissingSalaryField  = "missing salary field in job request"
+	ErrMissingSkillsField  = "missing skills field in job request"
+)
+
 type IValidatorService interface {
 	Validate(jobRequest *request.JobRequest) error
 }
@@ -18,16 +25,18 @@ func NewJobValidatorService() IValidatorService {
 
 func (s *JobValidatorService) Validate(jobRequest *request.JobRequest) error {
 	if jobRequest.Name == "" {
-		return fmt.Errorf("missing name field in job request")
+		return fmt.Errorf(ErrMissingNameField)
 	}
 	if jobRequest.Country == "" {
-		return fmt.Errorf("missing country field in job request")
+		return fmt.Errorf(ErrMissingCountryField)
 	}
 	if jobRequest.Salary == 0 {
-		return fmt.Errorf("missing salary field in job request")
+
+		return fmt.Errorf(ErrMissingSalaryField)
 	}
 	if len(jobRequest.Skills) == 0 {
-		return fmt.Errorf("missing skills field in job request")
+
+		return fmt.Errorf(ErrMissingSkillsField)
 	}
 	return nil
 }
---
+++ internal/service/validatorService_test.go
@@ -0,0 +1,76 @@
+package service_test
+
+import (
+	"errors"
+	"testing"
+	"v0/internal/service"
+
+	"github.com/stretchr/testify/assert"
+	"v0/internal/delivery/http/request"
+)
+
+func TestJobValidatorService_Validate(t *testing.T) {
+	tests := []struct {
+		name        string
+		jobRequest  *request.JobRequest
+		expectedErr error
+	}{
+		{
+			name: "Valid job request",
+			jobRequest: &request.JobRequest{
+				Name:    "Software Engineer",
+				Salary:  5000,
+				Country: "USA",
+				Skills:  []string{"Go", "Java", "Python"},
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "Missing name field",
+			jobRequest: &request.JobRequest{
+				Salary:  5000,
+				Country: "USA",
+				Skills:  []string{"Go", "Java", "Python"},
+			},
+			expectedErr: errors.New(service.ErrMissingNameField),
+		},
+		{
+			name: "Missing country field",
+			jobRequest: &request.JobRequest{
+				Name:   "Software Engineer",
+				Salary: 5000,
+				Skills: []string{"Go", "Java", "Python"},
+			},
+			expectedErr: errors.New(service.ErrMissingCountryField),
+		},
+		{
+			name: "Missing salary field",
+			jobRequest: &request.JobRequest{
+				Name:    "Software Engineer",
+				Country: "USA",
+				Skills:  []string{"Go", "Java", "Python"},
+			},
+			expectedErr: errors.New(service.ErrMissingSalaryField),
+		},
+		{
+			name: "Missing skills field",
+			jobRequest: &request.JobRequest{
+				Name:    "Software Engineer",
+				Salary:  5000,
+				Country: "USA",
+			},
+			expectedErr: errors.New(service.ErrMissingSkillsField),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			validator := service.NewJobValidatorService()
+
+			err := validator.Validate(test.jobRequest)
+
+			assert.Equal(t, test.expectedErr, err)
+		})
+	}
+}
